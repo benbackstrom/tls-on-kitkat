657beaef89e58ccc6d8addf0ba84eac7bbe1d051
Ben Backstrom <benbackstrom@gmail.com>
2017-01-15 09:00:31 -0800

    First attempt at a request on the webview with TLS

---
+++ app/src/main/java/com/recursivecase/ben/tlsapp/BetterSocketFactory.java
@@ -0,0 +1,78 @@
+package com.recursivecase.ben.tlsapp;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+/**
+ * @author fkrauthan
+ */
+public class BetterSocketFactory extends SSLSocketFactory {
+
+    private SSLSocketFactory factory;
+
+    public BetterSocketFactory(Context context) {
+        try {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, null, null);
+            factory = sslContext.getSocketFactory();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+            Toast.makeText(context, "KeyManagementException", Toast.LENGTH_SHORT).show();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Socket enableTLSForSocket(Socket socket) {
+        if(socket != null && (socket instanceof SSLSocket)) {
+            ((SSLSocket)socket).setEnabledProtocols(new String[] {"TLSv1.1", "TLSv1.2"});
+        }
+        return socket;
+    }
+
+    @Override
+    public String[] getDefaultCipherSuites() {
+        return factory.getDefaultCipherSuites();
+    }
+
+    @Override
+    public String[] getSupportedCipherSuites() {
+        return factory.getSupportedCipherSuites();
+    }
+
+    @Override
+    public Socket createSocket(Socket s, String host, int port, boolean autoClose) throws IOException {
+        return enableTLSForSocket(factory.createSocket(s, host, port, autoClose));
+    }
+
+    @Override
+    public Socket createSocket(String host, int port) throws IOException, UnknownHostException {
+        return enableTLSForSocket(factory.createSocket(host, port));
+    }
+
+    @Override
+    public Socket createSocket(String host, int port, InetAddress localHost, int localPort) throws IOException, UnknownHostException {
+        return enableTLSForSocket(factory.createSocket(host, port, localHost, localPort));
+    }
+
+    @Override
+    public Socket createSocket(InetAddress host, int port) throws IOException {
+        return enableTLSForSocket(factory.createSocket(host, port));
+    }
+
+    @Override
+    public Socket createSocket(InetAddress address, int port, InetAddress localAddress, int localPort) throws IOException {
+        return enableTLSForSocket(factory.createSocket(address, port, localAddress, localPort));
+    }
+}
--- app/src/main/java/com/recursivecase/ben/tlsapp/MainActivity.java
+++ app/src/main/java/com/recursivecase/ben/tlsapp/MainActivity.java
@@ -2,12 +2,76 @@ package com.recursivecase.ben.tlsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.button);
+        mWebView = (WebView) findViewById(R.id.webview);
+
+        try {
+            HttpsURLConnection.setDefaultSSLSocketFactory(new BetterSocketFactory(this));
+            mWebView.setWebViewClient(new HttpCallBackClient());
+        } catch(Throwable t) {
+            t.printStackTrace();
+        }
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWebView.loadUrl("https://www.google.com/");
+            }
+        });
+    }
+
+    private class HttpCallBackClient extends WebViewClient {
+        @Override
+        public WebResourceResponse shouldInterceptRequest(WebView view, String urlString) {
+            WebResourceResponse response = null;
+            URL url;
+            InputStream stream = null;
+            HttpsURLConnection connection = null;
+            try {
+                url = new URL(urlString);
+                connection = (HttpsURLConnection) url.openConnection();
+                connection.setReadTimeout(3000);
+                connection.setConnectTimeout(3000);
+                connection.setRequestMethod("GET");
+                connection.setDoInput(true);
+                connection.connect();
+                int responseCode = connection.getResponseCode();
+                if (responseCode != HttpsURLConnection.HTTP_OK) {
+                    throw new IOException("HTTP error code: " + responseCode);
+                }
+                String mimeType = connection.getContentType();
+                response = new WebResourceResponse(mimeType, "utf-8", connection.getInputStream());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        }
     }
 }
