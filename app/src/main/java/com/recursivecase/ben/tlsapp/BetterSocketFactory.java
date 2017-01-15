d3c37e28d0c8759f55316c1459d6f31ba6157772
Ben Backstrom <benbackstrom@gmail.com>
2017-01-15 14:52:52 -0800

    Only use TLS 1.2

--- app/src/main/java/com/recursivecase/ben/tlsapp/BetterSocketFactory.java
+++ app/src/main/java/com/recursivecase/ben/tlsapp/BetterSocketFactory.java
@@ -36,7 +36,7 @@ public class BetterSocketFactory extends SSLSocketFactory {
 
     private Socket enableTLSForSocket(Socket socket) {
         if(socket != null && (socket instanceof SSLSocket)) {
-            ((SSLSocket)socket).setEnabledProtocols(new String[] {"TLSv1.1", "TLSv1.2"});
+            ((SSLSocket)socket).setEnabledProtocols(new String[] {"TLSv1.2"});
         }
         return socket;
     }
